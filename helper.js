a5b6ea1390cf79d975a467cc9aba323627d73c93
JeffreyvanderKooy <jeffreydebruijn1999@gmail.com>
2025-01-23 16:53:43 +0100

    Initial Commit

---
+++ helper.js
@@ -0,0 +1,92 @@
+export async function getAllUsers(db) {
+  const query = 'SELECT id, name FROM users;';
+
+  try {
+    const res = await db.query(query);
+    return res.rows;
+  } catch (err) {
+    console.error('Error executing query:', err);
+  }
+}
+
+export async function getUser(name, pin, db) {
+  const query = `
+    SELECT * FROM users
+    WHERE name = $1 AND pin = $2;
+  `;
+
+  const values = [name, pin]; // Use parameterized queries to prevent SQL injection
+
+  try {
+    const res = await db.query(query, values);
+
+    const [user] = res.rows;
+
+    if (!user) throw new Error('No user found! Have you made an account yet?');
+
+    return user;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export async function insertUser(name, pin, db) {
+  const query = `
+    INSERT INTO users (name, pin)
+    VALUES ($1, $2)
+    RETURNING id, name;
+  `;
+  const values = [name, pin];
+
+  try {
+    const res = await db.query(query, values);
+    return res;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export async function getItems(raid, db) {
+  const query = `
+    SELECT 
+      id, 
+      name, 
+      item, 
+      TO_CHAR(date, 'DD/MM/YY, HH24:MI') AS formatted_date 
+    FROM ${raid};
+  `;
+
+  try {
+    const res = await db.query(query);
+    return res;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export async function submitItem(data, db) {
+  const { id, item, name, raid } = data;
+
+  const query = `
+    INSERT INTO ${raid} (id, item, name)
+    VALUES ($1, $2, $3)
+     RETURNING id, name, item, TO_CHAR(date, 'DD/MM/YY, HH24:MI') AS formatted_date;
+  `;
+
+  const values = [id, item, name];
+
+  try {
+    const res = await db.query(query, values);
+    return res;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export function capitalize(str) {
+  return str
+    .toLowerCase()
+    .split('_')
+    .map(item => item.replace(item[0], item[0].toUpperCase()))
+    .join(' ');
+}
---
+++ server.js
@@ -0,0 +1,122 @@
+import {
+  getAllUsers,
+  getUser,
+  capitalize,
+  getItems,
+  submitItem,
+  insertUser,
+} from './helper.js';
+
+import express from 'express';
+import pg from 'pg';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+const app = express();
+const port = 3000;
+
+dotenv.config(); // init dotenv
+
+// Only allow requests from this server
+const allowedOrigin = process.env.CORS_ALLOWED_ORIGIN;
+app.use(
+  cors({
+    origin: allowedOrigin,
+  })
+);
+// postgress data //
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+});
+
+db.connect(); // connect database
+
+// Gets all users on pageload
+app.get('/users', async (_, res) => {
+  try {
+    const users = await getAllUsers(db);
+
+    res.send({ ok: true, data: users });
+  } catch (error) {
+    res.status(400).send({ ok: false, message: error.message });
+  }
+});
+
+// Gets all items reserved for given raid
+app.get('/raid', async (req, res) => {
+  try {
+    const { raid } = req.query;
+
+    if (!raid)
+      throw new Error('Could not find a target raid. Please try again.');
+
+    const { rows } = await getItems(raid, db);
+
+    res.send({ ok: true, data: { raid: raid, rows } });
+  } catch (error) {
+    res.status(400).send({ ok: false, message: error.message });
+  }
+});
+
+// Login function
+app.post('/login', async (req, res) => {
+  try {
+    const { name, pin } = req.query;
+
+    if (pin.length !== 4) throw new Error('Pin must be 4 characters long.');
+    if (!name) throw new Error('Please enter your ingame character name.');
+
+    const user = await getUser(name.toLowerCase(), pin, db);
+
+    res.send({ ok: true, data: user });
+  } catch (error) {
+    res.status(400).send({ ok: false, message: error.message });
+  }
+});
+
+// register function
+app.post('/register', async (req, res) => {
+  try {
+    const { name, pin } = req.query;
+
+    if (pin.length !== 4) throw new Error('Pin must be 4 characters long.');
+    if (!name) throw new Error('Please enter your ingame character name.');
+
+    const { rows: user } = await insertUser(name.toLowerCase(), pin, db);
+
+    res.send({ ok: true, data: user });
+  } catch (error) {
+    res.status(400).send({ ok: false, message: error.message });
+  }
+});
+
+// Reserves a item for given raid
+app.post('/reserve', async (req, res) => {
+  try {
+    const { item, id, name, raid } = req.query;
+
+    if (!item || !id || !raid || !name)
+      throw new Error('Oops something went wrong!');
+
+    const data = {
+      item: capitalize(item),
+      id,
+      raid,
+      name: capitalize(name),
+    };
+
+    const result = await submitItem(data, db);
+
+    res.send({ ok: true, result });
+  } catch (error) {
+    res.status(400).send({ ok: false, message: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log('Site hosting on port: ' + port);
+});
