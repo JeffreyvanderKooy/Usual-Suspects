3d898f8189d068ed26096cd1b9b6345e378307b6
JeffreyvanderKooy <jeffreydebruijn1999@gmail.com>
2025-01-23 16:59:39 +0100

    Updated port constant to match Renders port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Render will provide the port in process.env.PORT
 
 dotenv.config(); // init dotenv
 
